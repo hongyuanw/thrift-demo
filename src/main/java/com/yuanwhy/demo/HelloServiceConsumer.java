d32f0de5031cfe974b84a6e80418d949407e4342
yuanwhy <henrywangwhy@gmail.com>
2017-04-08 15:46:34 +0800

    catch TApplicationException.MISSING_RESULT

--- src/main/java/com/yuanwhy/demo/HelloServiceConsumer.java
+++ src/main/java/com/yuanwhy/demo/HelloServiceConsumer.java
@@ -1,6 +1,9 @@
 package com.yuanwhy.demo;
 
 import com.yuanwhy.service.HelloService;
+import org.apache.thrift.TApplicationException;
+import org.apache.thrift.TEnum;
+import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
@@ -16,10 +19,17 @@ public class HelloServiceConsumer {
             transport.open();
             TProtocol protocol = new TBinaryProtocol(transport);
             HelloService.Client client = new HelloService.Client(protocol);
-            System.out.println(client.sayHello("Hongyuan"));
+            System.out.println(client.sayHello("yuanwhy"));
             transport.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (TApplicationException e) {
+
+            if (e.getType() == TApplicationException.MISSING_RESULT) {
+                System.out.println("null");
+            }
+
+        } catch (TException e){
+
         }
     }
+
 }
